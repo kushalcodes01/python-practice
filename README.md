1092a15aa0f55e9ab33a9b41dbc8653107cb5838
Kushal Lamsal <kushal.lamsal111@gmail.com>
2026-06-29 18:57:31 +0545

    Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Practice
+
+This repository contains my Python learning journey.
+
+## Programs
+- Hello World
+- Calculator
+
+Created by Kushal Lamsal.
--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
 num1=float(input("Enter the forst number:"))
 num2=float(input("Enter the second number:"))
 
-print(f"Sum=,{num1+num2}")
+print("Sum=", num1+num2)
