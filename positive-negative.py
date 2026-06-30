f2d37459eb58073b91911c4b4254dbebbe9e67ae
Kushal Lamsal <kushal.lamsal111@gmail.com>
2026-06-30 22:33:04 +0545

    Added poaitive negative tester

---
+++ positive-negative.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number:"))
+
+if num > 0:
+    print("The number is Positive..")
+elif num < 0:
+    print("The Number is Negative..")
+else:
+    print("Obviously it is zero..")
